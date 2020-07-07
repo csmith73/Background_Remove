877114907ec8698c661a62aea930729cda90a494
Carter <cartersmith73@gmail.com>
2020-07-06 21:22:38 -0400

    Good working version 7_6_2020. Speed issue fixed by only returning array. (This is for the local version 127.0.0.7:5001)

--- app.py
+++ app.py
@@ -207,5 +207,5 @@ def remove_background_api():
 
 
 if __name__ == '__main__':
-    #app.run(host='127.0.0.1', port='5001')
-    app.run(host='0.0.0.0')
+    app.run(host='127.0.0.1', port='5001', debug=True)
+    #app.run(host='0.0.0.0')
